d90653d67ae49bb7e55b7dd365cf92326135f6c0
David Svensson <davsven@beda.nfs.private>
2015-02-17 17:32:39 +0100

    mög

--- bulkAl.py
+++ bulkAl.py
@@ -3,22 +3,26 @@ from __future__ import print_function
 from ase import Atoms
 from ase.visualize import view
 from gpaw import GPAW, PW
+from ase.lattice.cubic import FaceCenteredCubic
 
 name = 'Al-fcc'
 a = 4.05  # fcc lattice paramter
 b = a / 2
 
-bulk = Atoms('Mg',
-             cell=[[b, 0, 0],
-                   [0, b, 0],
-                   [0, 0, b]],
-             pbc=True)
+#bulk = Atoms('Mg',
+#             cell=[[b, 0, 0],
+#                   [0, b, 0],
+#                   [0, 0, b]],
+#             pbc=True)
 
-view(bulk)
+bulk = FaceCenteredCubic(directions=[[1,0,0], [0,1,0], [0,0,1]],
+                          size=(2,2,2), symbol='Al', pbc=(1,1,1))
+
+#view(bulk)
 
 k = 8
 calc = GPAW(mode=PW(500),       # cutoff
-            kpts=(k, k, k),     # k-points
+            #kpts=(k, k, k),     # k-points
             txt=name + '.txt')  # output file
 
 bulk.set_calculator(calc)
