386d85f8ba5596569535a11c5c35fbc7c4a3154b
Josephine Cuellar Andersson <josander@beda.nfs.private>
2015-02-18 17:24:10 +0100

    Results for Al 100

--- bulkSC.py
+++ bulkSC.py
@@ -7,9 +7,16 @@ from gpaw.eigensolvers import Davidson
 
 name = '100-SC'
 a = 4.00  # Lattice paramter for Mg
+nbrPrimCells = 4
 
-while (a < 5.0):
-	b = a / 2
+k = 6
+calc = GPAW(mode=PW(300),       # cutoff
+	         kpts=(k, k, k),     # k-points
+	         txt=name + '.txt',	# output file
+		 eigensolver='dav') 
+
+
+while (a <= 5.0):
 
 	bulk = Atoms(['Al','Al','Al','Al'], 
 		     positions=[(0,0,0),
@@ -19,19 +26,13 @@ while (a < 5.0):
 	             cell=[2*a, a, 2*a],
 	             pbc=True)
 
-	k = 8
-	calc = GPAW(mode=PW(300),       # cutoff
-	            kpts=(k, k, k),     # k-points
-	            txt=name + '.txt',	# output file
-		    eigensolver='dav') 
-
 	bulk.set_calculator(calc)
 
 	energy = bulk.get_potential_energy()
 
-	energy = energy/4
+	energy = energy/nbrPrimCells
 
 	calc.write(name + '.gpw')
-	print('Energy:', energy, 'eV')
+	print("%f \t %f" %(energy, a))
 
 	a = a + 0.1
